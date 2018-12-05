7f20fdd161f32f90652bd734b74f977c22d9025b
Mahmoud_Saad <msammamsa10@gmail.com>
2018-12-06 00:27:09 +0200

    0000

--- FREERTOS/FREERTOS_V9.0.0_atmega32_Z_SecondShoot/FREERTOS_V9.0.0_atmega32_Z_SecondShoot/main.c
+++ FREERTOS/FREERTOS_V9.0.0_atmega32_Z_SecondShoot/FREERTOS_V9.0.0_atmega32_Z_SecondShoot/main.c
@@ -12,25 +12,24 @@
 
 void task1(void * pv);
 void task2(void * pv);
-	xQueueHandle queue_1_handle;
+xQueueHandle queue_1_handle;
 void main(void)
 {
 	DDRA=0xff;		DDRB=0xff;		DDRC=0xff;		DDRD=0xff;
 	char msa=0xff;
 	queue_1_handle=xQueueCreate( 2,sizeof(int));
-	if (queue_1_handle == NULL)
-		PORTD=0xff;
-	//{
-		
-		xTaskCreate(task1,NULL,configMINIMAL_STACK_SIZE,(void *)&msa,1,NULL);
-		xTaskCreate(task2,NULL,configMINIMAL_STACK_SIZE,(void *)&msa,1,NULL);
-		
-		vTaskStartScheduler();
-	//} 
-	//else //couldn't create a queue
-	//{
-	//	PORTA=PORTB=PORTC=PORTD=0x55;
-//	}
+	if (queue_1_handle != NULL)
+	{
+	
+	xTaskCreate(task1,NULL,configMINIMAL_STACK_SIZE,(void *)&msa,1,NULL);
+	xTaskCreate(task2,NULL,configMINIMAL_STACK_SIZE,(void *)&msa,1,NULL);
+	
+	vTaskStartScheduler();
+	}
+	else //couldn't create a queue
+	{
+		PORTA=PORTB=PORTC=PORTD=0x55;
+	}
 	
 }
 void task1(void * pv)
@@ -62,6 +61,61 @@ void task2(void * pv)
 	}
 }
 
+
+
+//Creating and using a queue
+/*
+void task1(void * pv);
+void task2(void * pv);
+	xQueueHandle queue_1_handle;
+void main(void)
+{
+	DDRA=0xff;		DDRB=0xff;		DDRC=0xff;		DDRD=0xff;
+	char msa=0xff;
+	queue_1_handle=xQueueCreate( 2,sizeof(int));
+	if (queue_1_handle != NULL)
+	{
+		
+		xTaskCreate(task1,NULL,configMINIMAL_STACK_SIZE,(void *)&msa,1,NULL);
+		xTaskCreate(task2,NULL,configMINIMAL_STACK_SIZE,(void *)&msa,1,NULL);
+		
+		vTaskStartScheduler();
+	}
+	else //couldn't create a queue
+	{
+		PORTA=PORTB=PORTC=PORTD=0x55;
+	}
+	
+}
+void task1(void * pv)
+{
+	char loc_val=(char)pv;
+	while(1)
+	{
+		PORTC ^=(1<<1);
+		PORTA=loc_val;
+		portBASE_TYPE stat= xQueueSendToBack(queue_1_handle,&loc_val,10);
+		loc_val++;
+		if (stat==pdFAIL)
+		{
+			PORTA=0xff;
+		}
+		vTaskDelay(100);
+	}
+}
+
+void task2(void * pv)
+{
+	portBASE_TYPE rx_buff=0;
+	while(1)
+	{
+		PORTC ^=(1<<2);
+		xQueueReceive(queue_1_handle,&rx_buff,10);
+		PORTB= rx_buff;
+		vTaskDelay(100);
+	}
+}
+*/
 
 //testing the running of v9.0.0
 /*
