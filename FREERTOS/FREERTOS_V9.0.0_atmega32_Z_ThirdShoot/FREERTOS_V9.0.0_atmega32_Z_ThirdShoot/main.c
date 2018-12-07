5f851cc0e48155d79c6c2bf0038bf60705a20a16
Mahmoud_Saad <msammamsa10@gmail.com>
2018-12-07 16:00:23 +0200

    0x03

--- FREERTOS/FREERTOS_V9.0.0_atmega32_Z_ThirdShoot/FREERTOS_V9.0.0_atmega32_Z_ThirdShoot/main.c
+++ FREERTOS/FREERTOS_V9.0.0_atmega32_Z_ThirdShoot/FREERTOS_V9.0.0_atmega32_Z_ThirdShoot/main.c
@@ -13,63 +13,25 @@
 
 //test-1 
 
-void TX_F(void * pv);
-void RX_F(void * pv);
-xQueueHandle queue_1_handle;
-typedef struct  
-{
-	portBASE_TYPE val_a;
-	portBASE_TYPE val_b;
-	portBASE_TYPE val_c;
-	portBASE_TYPE *ptr_name;
-}xData_t;
+void TASK_F(void * pv);
+
 void main(void)
 {
-	xData_t data_arr[2]={{2,4,6,"MSA"},{3,5,7,"MMA"}};
 	DDRA=0xff;		DDRB=0xff;		DDRC=0xff;		DDRD=0xff;
-	char msa=0xff;
-	queue_1_handle=xQueueCreate( 3,sizeof(xData_t));
-	if (queue_1_handle != NULL)
-	{
-	
-	xTaskCreate(TX_F,NULL,configMINIMAL_STACK_SIZE,(void *)&data_arr[0],2,NULL);
-	xTaskCreate(TX_F,NULL,configMINIMAL_STACK_SIZE,(void *)&data_arr[1],2,NULL);
-	xTaskCreate(RX_F,NULL,configMINIMAL_STACK_SIZE,NULL,1,NULL);
+	xTaskCreate(TASK_F,NULL,configMINIMAL_STACK_SIZE,(void *)&data_arr[0],2,NULL);
 	
 	vTaskStartScheduler();
-	}
-	else //couldn't create a queue
-	{
-		PORTA=PORTB=PORTC=PORTD=0x55;
-	}
 	
 }
-void TX_F(void * pv)
+void TASK_F(void * pv)
 {
 	
 	while(1)
 	{
 		PORTC ^=(1<<1);
-		portBASE_TYPE stat= xQueueSendToBack(queue_1_handle,pv,10);
-		if (stat==pdFAIL)
-		{
-			PORTA=0xff;
-		}
+		
 		taskYIELD();
 		//vTaskDelay(100/portTICK_RATE_MS);
-	}
-}
-
-
-void RX_F(void * pv)
-{
-	xData_t rx_buff;
-	while(1)
-	{
-		PORTC ^=(1<<2);
-		xQueueReceive(queue_1_handle,&rx_buff,10);
-		PORTB= rx_buff.val_a;
-	
 	}
 }
 
