765cdbde3f0fbcc7ba8d368bd0e80bf26934a674
Mahmoud_Saad <msammamsa10@gmail.com>
2018-12-07 15:34:23 +0200

    0x00

---
+++ FREERTOS/FREERTOS_V9.0.0_atmega32_Z_ThirdShoot/FREERTOS_V9.0.0_atmega32_Z_ThirdShoot/main.c
@@ -0,0 +1,18 @@
+/*
+ * FREERTOS_V9.0.0_atmega32_Z_ThirdShoot.c
+ *
+ * Created: 07/12/2018 15:32:09
+ * Author : Mahmoud
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
